2ac7e6ce08a5aacc5c40e95b8249ebd9775c2492
cevdetozbey <cevozby@hotmail.com>
2025-05-07 00:07:11 +0300

    buff update

---
+++ Assets/Script/Buffs/BuffSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class BuffSpawner : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] List<BuffSO> buffs;
+    [SerializeField] GameObject buffPrefab;
+    [SerializeField] float spawnInterval = 5f;
+    [SerializeField] float spawnRadius = 5f;
+
+    [SerializeField] Transform parent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnBuff", spawnInterval, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnBuff()
+    {
+        if(lineRenderer.positionCount == 0 || buffPrefab == null)
+            return;
+
+        int randomIndex = Random.Range(0, lineRenderer.positionCount);
+        Vector3 spawnPosition = lineRenderer.GetPosition(randomIndex) + Random.insideUnitSphere * spawnRadius;
+        spawnPosition.z = 0; // Ensure the buff is spawned on the same plane as the line renderer
+        GameObject buff = Instantiate(buffPrefab, spawnPosition, Quaternion.identity, parent);
+    }
+
+}
